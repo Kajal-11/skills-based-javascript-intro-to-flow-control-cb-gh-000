b819b052f352b6b41d73c897d71bbdd85a608952
Kajal Goel <kajal11122000@gmail.com>
2019-06-14 11:57:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -40,6 +40,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  switch(age){
   case 13:
   case 14:
   case 15:
@@ -47,7 +48,8 @@ function switchAge(age) {
   case 17:
   case 18:
   case 19:
-  return("You are a teenager");
+  return "You are a teenager"
   default:
   return 'You have an age'
+  }
 }
